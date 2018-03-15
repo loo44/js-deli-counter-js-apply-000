4e62b043c3f403485728d71b8948c763dc129776
Luis Ruiz <giosterr44@gmail.com>
2018-03-15 14:05:59 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,25 @@
+var katzDeli = [];
+ function takeANumber (string) {
+   katzDeliLine();
+   //has to go along with the new person's name as parameters
+   //the function should return their position in line
+ }
+ 
+ function nowServing() {
+   return ();//the first person in line then remove them 
+   if ();//noone in line
+   {
+     return ("There is nobody waiting to be served!");
+   }
+ }
+ 
+ function currentLine(string) {
+   if (katzDeliLine("Ada","Grace"));
+   {
+     return string==="The line is currently : 1. Ada, 2. Grace"
+   }
+   else ()
+   {
+     return string===("The line is currently empty.")
+   }
+ }
